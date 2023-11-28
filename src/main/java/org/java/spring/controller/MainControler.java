42068864388867294c306e75c71f975a044976c6
EugeniuPeruzzi <jaka1997@live.it>
2023-11-28 13:04:21 +0100

    created Movie and Song model

--- src/main/java/org/java/spring/controller/MainControler.java
+++ src/main/java/org/java/spring/controller/MainControler.java
@@ -1,11 +1,21 @@
 package org.java.spring.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.java.spring.model.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainControler {
+	
+	private List<Song> getBestSong(){
+		return null;
+		
+	}
+	
 	
 	//passaggio di una variabile al html
 	@GetMapping("/")
