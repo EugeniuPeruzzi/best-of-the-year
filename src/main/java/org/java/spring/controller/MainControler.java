a11ede84a7f9b3d4b6ffabe5b9ff457ee396bddf
EugeniuPeruzzi <jaka1997@live.it>
2023-11-28 12:47:18 +0100

    step 1 done

--- src/main/java/org/java/spring/controller/MainControler.java
+++ src/main/java/org/java/spring/controller/MainControler.java
@@ -1,5 +1,19 @@
 package org.java.spring.controller;
 
-public class MainControler {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class MainControler {
+	
+	//passaggio di una variabile al html
+	@GetMapping("/")
+	public String myName(Model model) {
+		
+		final String username = "Eugeniu";
+		
+		model.addAttribute("username", username);
+		return "index";
+	}
 }
